819f371d160066afe77f4c9546266c48add04ed2
Spatil <sahilpatil67049@gmail.com>
2024-12-05 16:13:18 +0530

    added database models

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const connectToDB = require("./dbConnect/dbConnect.js");
 
 dotenv.config({})
-
 const port = process.env.PORT || 3000;
+
+connectToDB();
 
 app.listen(port, ()=>{
     console.log(`app listen on port ${port}`);
-})
+});
